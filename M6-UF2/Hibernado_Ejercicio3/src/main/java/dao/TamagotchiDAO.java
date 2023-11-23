7b18ef32276ccb0d01299eae39b8945431229703
Pchaozz <polsh2001.1@gmail.com>
2023-11-23 15:30:24 +0100

    Update TamagotchiDAO.java
    - Creado el constructor del TamagotchiDAO y el equipar Joguina

--- M6-UF2/Hibernado_Ejercicio3/src/main/java/dao/TamagotchiDAO.java
+++ M6-UF2/Hibernado_Ejercicio3/src/main/java/dao/TamagotchiDAO.java
@@ -1,5 +1,17 @@
 package dao;
 
-public class TamagotchiDAO {
+import models.Joguina;
+import models.Tamagotchi;
 
+public class TamagotchiDAO extends GenericDAO<Tamagotchi, Integer>{
+
+	public TamagotchiDAO() {
+		super(Tamagotchi.class);
+	}
+	
+	public void EquiparJoguina (Joguina j, int id) {
+		Tamagotchi t = find(id);
+		t.setJoguina(j);
+		update(t);
+	}
 }
