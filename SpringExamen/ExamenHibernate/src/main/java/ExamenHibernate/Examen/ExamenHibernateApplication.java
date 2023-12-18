d75703732fe709d2264fcc53ba0125f52a24f5eb
Pchaozz <polsh2001.1@gmail.com>
2023-12-18 14:23:24 +0100

    A SALIR CORRIENDO DE CASA

---
+++ SpringExamen/ExamenHibernate/src/main/java/ExamenHibernate/Examen/ExamenHibernateApplication.java
@@ -0,0 +1,13 @@
+package ExamenHibernate.Examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenHibernateApplication.class, args);
+	}
+
+}
---
+++ SpringExamen/ExamenHibernate/src/test/java/ExamenHibernate/Examen/ExamenHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package ExamenHibernate.Examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
